221fe7beff1824b56867af1aea0b5218f019d4e6
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-02-01 22:48:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,19 +6,19 @@ class App < Sinatra::Base
     
   end
   
-  get '/square/:number'
+  get '/square/:number' do
   
   end
   
-  get '/say/:number/:phrase'
+  get '/say/:number/:phrase' do 
   
   end
   
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
   
   end
   
-  get '/:operation/:number1/:number2'
+  get '/:operation/:number1/:number2' do
   
   end
   
