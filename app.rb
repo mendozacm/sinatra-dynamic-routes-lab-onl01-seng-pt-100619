e77c03ebb3685efa22f1389f0af3ec93da661720
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-02-01 23:20:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,8 +19,13 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-  '#{[params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]].join(' ')'
-  "'#{['12','34','35','231'].join("', '")}'"
+  @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+
   end
   
   get '/:operation/:number1/:number2' do
