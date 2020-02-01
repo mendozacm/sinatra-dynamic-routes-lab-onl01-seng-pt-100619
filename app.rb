1506f398c47e207f5755dc8c5a5c707bf497d2bb
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-02-01 22:53:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   
   get '/square/:number' do
   @product = params[:num1].to_i * params[:num1].to_i
-  	"#{@product}"
+  	#{@product}to.s
   end
   
   get '/say/:number/:phrase' do 
