a1a9b65b0fbcc2691b9defe4a348ccf0b861aedc
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-02-01 22:59:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,9 @@ class App < Sinatra::Base
   end
   
   get '/say/:number/:phrase' do 
-  
-  @phrase = params[:phrase]
   @number = params[:number].to_i
+  @phrase = params[:phrase]
+  
    
   end
   
